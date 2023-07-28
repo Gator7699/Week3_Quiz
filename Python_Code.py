d26b86712875bfe6e8d6b85472cb27f3ffe81deb
Hayden <haydenlennox52@gmail.com>
2023-07-28 11:07:49 +1000

    Second commit

--- Python_Code.py
+++ Python_Code.py
@@ -7,3 +7,28 @@ n = 7**2 +1
 for i in range (1,n+1):
   print(i)
   i = i+1
+
+
+class Engineer1():
+  def __init__(self):
+   super().__init__()
+  self.skill = "problem solver"
+  self.name = "Hayden"
+  self.type = "Mechatronic"
+  self.experience = 0
+
+class Engineer2():
+  def __init__(self):
+  super().__init__()
+  self.skill = "problem solver"
+  self.name = "Bill"
+  self.type = "Electrical"
+  self.experience = 20
+
+
+stringlength=len(name) # calculate length of the list
+slicedString=name[stringlength::-1] # slicing 
+print (slicedString) # print the reversed string
+
+
+  
